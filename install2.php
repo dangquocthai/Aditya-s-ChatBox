51da917a8647d239ac7d02297f30f22b97312652
Aditya Goturu <aditya3098@gmail.com>
2012-08-25 16:29:42 +0530

    Add GPL. Muhahahahaha.... No propritary versions now, eh!

--- install2.php
+++ install2.php
@@ -1,6 +1,19 @@
 <html>
 <body>
 <?php
+//		This file is part of Aditya's ChatBox
+// 	Aditya's ChatBox is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # Check if config file exists, and if it does, don't run for security reasons.
 # Although the install form has this already, a cracker could create his own
 $filename = 'includes/config.db.php';
